32affdd485977142b945a592eaf9d43554e550ff
AjDep <w.anjanadep@gmail.com>
2026-01-06 13:11:33 +0530

    update Kmeans

--- ML-Models/KMeans/result.py
+++ ML-Models/KMeans/result.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from predict import predict_today
-df_today = pd.read_csv("DataSets/today.csv")
+df_today = pd.read_csv("../DataSets/today1.csv")
 
 result = predict_today(df_today)
 
-watchlist = result[result["cluster"] == 3]  # Whale Accumulation
+watchlist = result[result["cluster"] == 2]  # Whale Accumulation
 
 print(watchlist[["security", "regime", "action"]])
--- ML-Models/KMeans/train.py
+++ ML-Models/KMeans/train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from market_regime_model import train_model, save_model
 
-df = pd.read_csv("DataSets/market-dashboard.csv")
+df = pd.read_csv("../DataSets/market-dashboard.csv")
 
 kmeans, scaler = train_model(df)
 save_model(kmeans, scaler)
